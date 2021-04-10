abeaa884bd4a84278f6a394b59a5f284e9718a3b
Tristan Chu <tjchu@ucsc.edu>
2021-04-10 00:00:02 -0700

    fully working List.c, just need poly.c

--- prog1/poly.c
+++ prog1/poly.c
@@ -3,11 +3,15 @@
 
 int main(void) {
   struct ListObj *new_list = newList();
-  appendList(new_list, 1);
-  appendList(new_list, 2);
-  appendList(new_list, 3);
+  appendList(new_list, 5);
+  appendList(new_list, 6);
+  appendList(new_list, 7);
   printList(new_list);
-  printf("element deleted: %d\n", delElement(new_list, 3));
-  printList(new_list);
+  printf("The biggest element is: %d\n", max(new_list));
+  printf("The element %d is at index %d\n", 8, find(new_list, 8));
+  // printList(new_list);
+  // printf("element deleted: %d\n", delElement(new_list, 1));
+  // printList(new_list);
+  freeList(&new_list);
   return 0;
 }
